c3f12c2df9a419b3f232f93a535809164fe510e8
Raul Morales <irmoralesb@hotmail.com>
2020-01-26 17:53:06 -0600

    C02 - Adding Author Controller

---
+++ BuildingRESTfulAPIASP.NETCore3/CourseLibrary/CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,38 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/authors")] //For Web APIs it is better only to have the same 
+    //[Route("api/[controller]")] //Not recommended since we want to keep the API the same, but evaluate it if that is what you want
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+
+        [HttpGet()]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+
+        [HttpGet("{authorId:guid}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId: authorId);
+            return new JsonResult(authorFromRepo);
+        }
+
+    }
+}
